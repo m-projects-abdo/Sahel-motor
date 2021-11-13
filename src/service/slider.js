c5360698ec78ace6f0cd569cfef7b12c52fb89b3
Mohammed Abdo <learningme42@gmail.com>
2021-11-13 03:54:51 +0200

    create and use new brand logo section

--- src/service/slider.js
+++ src/service/slider.js
@@ -1,23 +1,27 @@
 // for product scrolling
-$(".products").owlCarousel({
-  loop: true,
-  margin: 10,
-  autoplay: true,
-  nav: true,
-  autoplayHoverPause: true,
-  navText: [
-    "<i class='bx bxs-left-arrow'></i>",
-    "<i class='bx bxs-right-arrow'></i>"
-  ],
-  responsive: {
-    0: {
-      items: 1
-    },
-    600: {
-      items: 2
-    },
-    1000: {
-      items: 4
+(()=> {
+
+  $(".products").owlCarousel({
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    nav: true,
+    autoplayHoverPause: true,
+    navText: [
+      "<i class='bx bxs-left-arrow'></i>",
+      "<i class='bx bxs-right-arrow'></i>"
+    ],
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 4
+      }
     }
-  }
-});
+  });
+  
+})();
